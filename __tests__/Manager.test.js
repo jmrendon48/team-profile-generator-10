edd03255b66834ffd418e7a356a7a54fbc79b6c5
AlexDeLarge <jmrendon48@gmail.com>
2021-02-21 02:15:41 -0800

    added Manager properties and methods

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,14 @@
+const { test, expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 
 test('gets Employee office number', () => {
-    const employee = new Manager('Joshua', 1, "jmrendon48@gmail.com", role, 1);
+    const employee = new Manager('Joshua', 1, "jmrendon48@gmail.com", 1);
 
     expect(employee.officeNumber).toEqual(1);
+});
+
+test('gets Employee role', () => {
+    const employee = new Manager('Joshua', 1, "jmrendon48@gmail.com", 1);
+
+    expect(employee.role).toBe('Manager');
 });
