bd8a34aeea8ab69767f81174ea93d8293b6bed6a
AlexDeLarge <jmrendon48@gmail.com>
2021-02-21 02:09:48 -0800

    revised Employee class and tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('creates a Employee object', () => {
-    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com", "Employee");
+    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com");
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
@@ -11,25 +11,25 @@ test('creates a Employee object', () => {
 });
 
 test('gets Employee name', () => {
-    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com", "Employee");
+    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com");
 
     expect(employee.name).toBe('Joshua');
 });
 
 test('gets Employee ID', () => {
-    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com", "Employee");
+    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com");
 
     expect(employee.id).toEqual(1);
 });
 
 test('gets Employee email', () => {
-    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com", "Employee");
+    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com");
 
     expect(employee.email).toBe('jmrendon48@gmail.com');
 });
 
 test('gets Employee role', () => {
-    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com", "Employee");
+    const employee = new Employee('Joshua', 1, "jmrendon48@gmail.com");
 
     expect(employee.role).toBe('Employee');
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 test('gets Employee office number', () => {
-    const employee = new Manager('Joshua', 1, "jmrendon48@gmail.com", "Employee", 1);
+    const employee = new Manager('Joshua', 1, "jmrendon48@gmail.com", role, 1);
 
     expect(employee.officeNumber).toEqual(1);
 });
